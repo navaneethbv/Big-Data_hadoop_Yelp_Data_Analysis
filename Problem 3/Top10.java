bf666d52f4e1c26a86566811ec261057537e36fc
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-09-28 13:48:05 -0500

    Update Top10.java

--- Problem 3/Top10.java
+++ Problem 3/Top10.java
@@ -5,7 +5,7 @@ import java.util.TreeMap;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.FloatWritable; 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
